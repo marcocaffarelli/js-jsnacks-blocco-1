3360531e804618df9e951478c62ba7e91d86a50b
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-12-04 18:02:41 +0100

    Snack5: scritte le variabili e iniziata la funzione
    Scritte le variabili e iniziata la funzione

--- main5.js
+++ main5.js
@@ -1,5 +1,13 @@
-// Scrivi una funzione che accetti tre argomenti:
-// un array e due numeri (“a” più piccolo di “b” e “b” grande al
-// massimo quanto il numero di elementi dell’array).
 // La funzione ritornerà un nuovo array con i valori che
 // hanno la posizione compresa tra “a” e “b”
+
+//variabili
+// un array e due numeri (“a” più piccolo di “b” e “b” grande al massimo quanto il numero di elementi dell’array).
+var piccolo = 1; //a
+var grande = 4; //b
+var listaNumeri = [2, 5, 7, 9];
+
+// Scrivi una funzione che accetti tre argomenti:
+function fusione(a, lista, b) {
+    
+}
