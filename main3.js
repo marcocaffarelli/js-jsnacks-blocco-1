d5f649c32a2194ee6e11d6d6389dfdc5be188d3f
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-12-04 16:59:46 +0100

    Snack3: creato 10 oggetti zucchina
    creato 10 oggetti zucchina

--- main3.js
+++ main3.js
@@ -1,5 +1,47 @@
-// Crea 10 oggetti che rappresentano una zucchina.
 // Dividi in due array separati le zucchine che misurano
 // meno o più di 15cm.
 // Infine stampa separatamente quanto pesano i due gruppi
 // di zucchine
+
+// Crea 10 oggetti che rappresentano una zucchina.
+var zucchinaChiaraFaenza = {
+    peso : 10,
+    lunghezza : 16
+};
+var zucchinaFirenze = {
+    peso : 12,
+    lunghezza : 11
+};
+var zucchinaRieti = {
+    peso : 7,
+    lunghezza : 8
+};
+var zucchinaRoma = {
+    peso : 18,
+    lunghezza : 13
+};
+var zucchinaTerni = {
+    peso : 3,
+    lunghezza : 20
+};
+var zucchinaPerugia = {
+    peso : 6,
+    lunghezza : 17
+};
+var zucchinaParma = {
+    peso : 7,
+    lunghezza : 23
+};
+var zucchinaViterbo= {
+    peso : 8,
+    lunghezza : 5
+};
+var zucchinaTorino = {
+    peso : 16,
+    lunghezza : 10
+};
+var zucchinaBolzano = {
+    peso : 11,
+    lunghezza : 1
+};
+
