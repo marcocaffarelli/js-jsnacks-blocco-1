d3f3109fc1fa08c883bd31d7ecc7da1b5b32588c
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-12-04 16:55:35 +0100

    Creata una funzione che inverte la stringa
    Creata una funzione che inverte la stringa

--- main2.js
+++ main2.js
@@ -1,2 +1,14 @@
 // Scrivi una funzione che accetti una stringa come
 // argomento e la ritorni girata (es. Ciao -> oaiC)
+
+//funzione che inverte il testo
+function invertiTesto(stringa) {
+    stringa = stringa.split("").reverse().join("");
+    console.log(stringa);
+   
+};
+
+//invocazione della funzione sulla stringa "ciao"
+invertiTesto("ciao");
+//invocazione della funzione sulla stringa "stringa"
+invertiTesto("stringa");
