6c360040f503f45667cd22001d05e23423e727c6
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-12-04 17:37:29 +0100

    Snack4: create 3 liste e un ciclo for per fondere gli elementi
    Create 3 liste e un ciclo for per fondere gli elementi delle prime due liste nella terza

--- main4.js
+++ main4.js
@@ -2,3 +2,17 @@
 //     numero di elementi) prendendo alternativamente gli
 //     elementi da uno e dall’altro
 //     es. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
+
+//array di numeri e lettere
+var listaLettere = ["a","b","c"];
+var listaNumeri = [1,2,3];
+//array mista in cui fondere gli elementi delle prime due liste
+var listaMista = [];
+
+//ciclo for che inserisce gli elementi delle prime due liste nella terza lista
+for (var i = 0; i < listaLettere.length; i++) {
+    listaMista.push(listaLettere[i], listaNumeri[i]);
+        
+ };
+console.log(listaMista);
+
