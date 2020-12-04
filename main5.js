fd3658a71d633fc2b881499176911334f45836ef
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-12-04 18:16:16 +0100

    Creata una funzione che unisce tre argomenti in un quarto
    Creata una funzione che unisce tre argomenti in un quarto

--- main5.js
+++ main5.js
@@ -1,13 +1,22 @@
-// La funzione ritornerà un nuovo array con i valori che
-// hanno la posizione compresa tra “a” e “b”
-
 //variabili
 // un array e due numeri (“a” più piccolo di “b” e “b” grande al massimo quanto il numero di elementi dell’array).
 var piccolo = 1; //a
 var grande = 4; //b
 var listaNumeri = [2, 5, 7, 9];
+var listaCompleta = [];
 
 // Scrivi una funzione che accetti tre argomenti:
-function fusione(a, lista, b) {
-    
-}
+function fusione(a, lista, b, lista2) {
+    //ciclo for per inserire gli elementi della lista in lista2
+    for (var i = 0; i < lista.length; i++) {
+        lista2.push(lista[i])  
+    }
+    // inserisco alla fine della lista2 b
+    lista2.push(b) 
+    // inserisco all'inizio della lista2 a
+    lista2.unshift(a)
+};
+
+// La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra “a” e “b”
+fusione(piccolo, listaNumeri, grande, listaCompleta)
+console.log(listaCompleta);
